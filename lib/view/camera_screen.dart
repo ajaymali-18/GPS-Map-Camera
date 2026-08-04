998207c72d016420b3fb4a0f6ce8b42e60c9472b
ajaymali-18 <work.ajay27@gmail.com>
2026-08-04 17:14:57 +0530

    map added

--- lib/view/camera_screen.dart
+++ lib/view/camera_screen.dart
@@ -1,7 +1,9 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 // import 'package:geolocator/geolocator.dart';
 // import 'package:geocoding/geocoding.dart';
 import 'package:my_app2/services/location_service.dart';
@@ -100,15 +102,9 @@ class _CameraScreenState extends State<CameraScreen> {
             right: 20,
 
             child: Container(
-
-
-
-
-
-
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                
+
                 children: [
                   // Small Map ------------>
                   ClipRRect(
@@ -116,22 +112,48 @@ class _CameraScreenState extends State<CameraScreen> {
                     child: SizedBox(
                       width: 90,
                       height: 90,
-
-                      child: Image.asset(
-                        "assets/images/img1.jpg", // Replace with your static map later
-                        fit: BoxFit.cover,
-                      ),
+                      child: position == null
+                          ? const Center(child: CircularProgressIndicator())
+                          : FlutterMap(
+                              options: MapOptions(
+                                initialCenter: LatLng(
+                                  position!.latitude,
+                                  position!.longitude,
+                                ),
+                                initialZoom: 14,
+                              ),
+                              children: [
+                                TileLayer(
+                                  urlTemplate:
+                                      'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                  userAgentPackageName: 'com.example.my_app2',
+                                ),
+                                MarkerLayer(
+                                  markers: [
+                                    Marker(
+                                      point: LatLng(
+                                        position!.latitude,
+                                        position!.longitude,
+                                      ),
+                                      width: 40,
+                                      height: 40,
+                                      child: const Icon(
+                                        Icons.location_pin,
+                                        color: Color.fromARGB(
+                                          255,
+                                          235,
+                                          101,
+                                          92,
+                                        ),
+                                        size: 35,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                     ),
                   ),
-
-
-
-
-
-
-
-
-
 
                   const SizedBox(width: 12),
 
